1b0eda5fb8902b7f7d1b186982135473d7a81cb5
Kaliman <aleksadnar@nearshoregm.com>
2020-02-26 09:55:24 +0100

    design changes

--- php/contact.php
+++ php/contact.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require '../vendor/autoload.php';
+
 if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
   $test = "/(content-type|bcc:|cc:|to:)/i";
@@ -7,15 +13,28 @@ if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject'])
       exit;
     }
   }
+    $mail = new PHPMailer;
 
-$headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
-    'Reply-To: ' . $_POST["email"] . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->ContentType = 'text/plain';
+    $mail->IsHTML(false);
+    $mail->Host = 'nearshoregm.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'office@nearshoregm.com';
+    $mail->Password = 'nearshoregm1234';
+    $mail->setFrom('office@nearshoregm.com');
+    $mail->addReplyTo($_POST['email']);
+    $mail->addAddress("office@nearshoregm.com");
+    $mail->Subject =  $_POST['subject'];
+    $mail->Body = $_POST['message'];
+    $mail->createHeader();
 
-  if(mail( "marko@nearshoregm.com", $_POST['subject'], $_POST['message'], $headers )){
-      echo "SUCCESS";
-  } else {
-      echo "ERROR";
-  }
+    if ($mail->send()) {
+        echo "SUCCESS";
+    } else {
+        echo "ERROR";
+    }
 }
 ?>
